9c698ee98cbd9b535c1698dde25a0f59cf6cbd5f
numerhero <469564715@qq.com>
2018-04-15 12:30:24 +0800

    Feat: 尝试接入style-loader

--- views/jsx/loginAndRegister.jsx
+++ views/jsx/loginAndRegister.jsx
@@ -4,8 +4,8 @@ import { Provider, connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import { Button } from 'antd';
-
-
+import styles from './loginAndRegister.less';
+import DocumentTitle from 'react-document-title';
 class LoginAndRegister extends Component {
   constructor(props) {
     super(props)
@@ -14,9 +14,9 @@ class LoginAndRegister extends Component {
 
 
   render() {
-    return (<div>
-        <Button>123</Button>
-      </div>);
+    return (<DocumentTitle title={'登陆&注册'}>
+        <div className={styles.container}></div>
+      </DocumentTitle>);
   }
 }
 
