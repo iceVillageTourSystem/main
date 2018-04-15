9cb3a916eb9de4a841e52feb142f666a62d24945
Your Name <1092339586@qq.com>
2018-04-15 23:11:52 +0800

    Fix: 信息页面编写完成

--- views/jsx/components/info.jsx
+++ views/jsx/components/info.jsx
@@ -6,12 +6,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import {Table, Button } from 'antd';
 import DocumentTitle from 'react-document-title';
 
-const columns = [{
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: text => <a href="javascript:;">{text}</a>,
-  }];
+const columns = [{title: 'Name', dataIndex: 'name', key: 'name'}];
   
 class Info extends Component {
   constructor(props) {
@@ -20,17 +15,43 @@ class Info extends Component {
   }
 
   state = {
-      columns: ['a','b'],
-      data: [{
-        name: 'z',
-        key: "1"
-      }]
+    epName: 'a',
+    address: 'a',
+    phone: 'a',
+    leader: 'a',
+    epNum: 'a',
   }
   
 
   render() {
     return (
-        <Table columns={columns} dataSource={this.state.data} />
+        <div>
+        <Table 
+            columns={[{title: 'epName', dataIndex: 'epName', key: '1'}]}
+            dataSource={[{epName: this.state.epName, key: "1" }]}
+            pagination={false} 
+            />
+        <Table 
+            columns={[{title: 'address', dataIndex: 'address', key: '1'}]}
+            dataSource={[{address: this.state.address, key: "1" }]}
+            pagination={false} 
+            />
+        <Table 
+            columns={[{title: 'phone', dataIndex: 'phone', key: '1'}]}
+            dataSource={[{phone: this.state.phone, key: "1" }]}
+            pagination={false} 
+            />
+        <Table 
+            columns={[{title: 'leader', dataIndex: 'leader', key: '1'}]}
+            dataSource={[{leader: this.state.leader, key: "1" }]}
+            pagination={false} 
+            />
+        <Table 
+            columns={[{title: 'epNum', dataIndex: 'epNum', key: '1'}]}
+            dataSource={[{epNum: this.state.epNum, key: "1" }]}
+            pagination={false} 
+            />
+        </div>
         // <div>
         //     12
         // </div>
