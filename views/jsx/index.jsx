69bcc3ed81570291c5664603f5abccbcfa862eaf
numerhero <469564715@qq.com>
2018-04-16 20:54:53 +0800

    Fix: 先一步步来

--- views/jsx/index.jsx
+++ views/jsx/index.jsx
@@ -3,10 +3,10 @@ import { render } from "react-dom";
 import { Provider, connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import Chart from './components/chart';
-import Info from './components/info';
-import Query from './components/query';
-import InputInfo from './components/input';
+// import Chart from './components/chart';
+// import Info from './components/info';
+// import Query from './components/query';
+// import InputInfo from './components/input';
 import styles from './index.less';
 
 import {Layout, Menu} from 'antd';
@@ -19,16 +19,16 @@ class Index extends Component {
    
   }
 
-
   render() {
     return (
     <Router>
         <Layout>
             <Sider>
               <Menu theme={"dark"}>
-                <MenuItem>
-                  <img src='../img/logo.svg'/>
-                </MenuItem>
+                <p className={styles.logoWrap}>
+                  <img className={styles.logo} src='../img/logo.svg'/>
+                  <span>黑龙江省冰雪旅游产业信息系统</span>
+                </p>
                 <MenuItem>
                   <Link to="/info">基本信息</Link>
                 </MenuItem>
@@ -46,10 +46,7 @@ class Index extends Component {
             <Layout>
               <Header>Header</Header>
               <Content>
-                <Route path="/info" component={Info} />
-                <Route path="/query" component={Query} />
-                <Route path="/chart" component={Chart} />
-                <Route path="/input" component={InputInfo} />
+               
               </Content>
             </Layout>
           </Layout>
@@ -59,3 +56,8 @@ class Index extends Component {
 }
 
 export default Index;
+
+// <Route path="/info" component={Info} />
+// <Route path="/query" component={Query} />
+// <Route path="/chart" component={Chart} />
+// <Route path="/input" component={InputInfo} />
