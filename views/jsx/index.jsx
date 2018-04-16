a7cbb2811cf825a471f0c3a90d6fd9740a94b1e2
Your Name <1092339586@qq.com>
2018-04-16 22:11:41 +0800

    Feat: 完成系统的基础布局

--- views/jsx/index.jsx
+++ views/jsx/index.jsx
@@ -9,43 +9,84 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 // import InputInfo from './components/input';
 import styles from './index.less';
 
-import {Layout, Menu} from 'antd';
+import {Layout, Menu, Icon} from 'antd';
 const { Header, Footer, Sider, Content } = Layout;
 const MenuItem = Menu.Item;
 
 class Index extends Component {
   constructor(props) {
     super(props)
-   
+    
+  }
+
+  state = {
+    collapsed: false
+  }
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
   }
 
   render() {
     return (
     <Router>
         <Layout>
-            <Sider>
-              <Menu theme={"dark"}>
-                <p className={styles.logoWrap}>
-                  <img className={styles.logo} src='../img/logo.svg'/>
-                  <span>黑龙江省冰雪旅游产业信息系统</span>
-                </p>
-                <MenuItem>
-                  <Link to="/info">基本信息</Link>
-                </MenuItem>
-                <MenuItem>
-                  <Link to="/query">查询数据</Link>
-                </MenuItem>
-                <MenuItem>
-                  <Link to="/chart">绘制图表</Link>
-                </MenuItem>
-                <MenuItem>
-                  <Link to="/input">提交数据</Link>
-                </MenuItem>
-              </Menu>
+            <Sider
+              width="256"
+              collapsible
+              trigger={null}
+              collapsed={this.state.collapsed}
+            >
+              <div className={styles.logoWrap}>
+                <img className={styles.logo} src='../img/logo.svg'/>
+                <h1 className={styles.logoDesc}>冰雪旅游信息系统</h1>
+              </div>
+              <div>
+                <Menu
+                  theme={"dark"}
+                  mode="inline"
+                  defaultSelectedKeys={['1']}
+                >
+                  
+                  <MenuItem key="1">
+                    <Link to="/info">
+                      <Icon type="book" />
+                      <span>基本信息</span>
+                    </Link>
+                  </MenuItem>
+                  <MenuItem key="2">
+                    <Link to="/query">
+                      <Icon type="pie-chart" />
+                      <span>查询数据</span>
+                    </Link>
+                  </MenuItem>
+                  <MenuItem key="3">
+                    <Link to="/chart">
+                      <Icon type="area-chart" />
+                      <span>绘制图表</span>
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link to="/input">
+                      <Icon type="edit" />
+                      <span>提交数据</span>
+                    </Link>
+                  </MenuItem>
+                </Menu>
+              </div>
             </Sider>
             <Layout>
-              <Header>Header</Header>
-              <Content>
+              <Header style={{ background: '#fff', padding: 0 }}>
+                <Icon
+                  style={{padding: '0 24px', cursor: 'pointer'}}
+                  className="trigger"
+                  type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                  onClick={this.toggle}
+                />
+              </Header>
+              <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
                
               </Content>
             </Layout>
